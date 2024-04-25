bdce451b515ceb917bc174f4b9937f87249e67c8
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-04-26 03:10:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here
+let touristSpots = ["The Virupaksha Temple", "Victoria Memorial", "Tajmahal"];
 
+document.addEventListener("DOMContentLoaded", () => {
+  function removeArticles(name) {
+    return name.replace(/^(a |an |the )/i, "");
+  }
+
+  touristSpots.sort((a, b) =>
+    removeArticles(a).localeCompare(removeArticles(b))
+  );
+
+  const bandList = document.getElementById("band");
+
+  bandList.innerHTML = "";
+
+  touristSpots.map((name) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = name;
+    bandList.appendChild(listItem);
+  });
+});
